05425603e6d8ae6d4cd385408744aa9577e01f4f
shulace13 <kozasa0820jps@gmail.com>
2018-10-31 19:14:03 -0400

    Worked once but not working again. Try and put some for structures in instead of that if thingy

--- shu_gazebo/src/circles_vel.cpp
+++ shu_gazebo/src/circles_vel.cpp
@@ -18,6 +18,9 @@
 
 #include <iostream>
 
+// #define VEL_MAX 0.3
+// #define VEL_MIN -0.3
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "circles_vel");
     ros::NodeHandle n;
@@ -27,35 +30,42 @@ int main(int argc, char **argv){
     double l_scale, a_scale;
     // int check = 0;
     bool check = false;
+    linear_ = 1;
+    l_scale = 0.0;
+    angular_ = 1;
+    a_scale = 0.3;
+     
+    vel.linear.x = linear_*l_scale;
+    vel.angular.z = angular_*a_scale;
 
     ros::Publisher vel_pub_ = n.advertise<geometry_msgs::Twist>("cmd_vel", 1, true);
     ros::Rate loop_rate(1);
 
+
     while(ros::ok()){
-        linear_ = 1;
-        l_scale = 0.1;
-        angular_ = 1;
-        a_scale = 0.3;
-        
-        vel.linear.x = linear_*l_scale;
-        vel.angular.z = angular_*a_scale;
 
-        if(vel.linear.x < 0.5 && check!=true){
+       if(vel.linear.x < 0.3  && check!=true){
             vel.linear.x += 0.1;
-            if(vel.linear.x==0.5){
+            printf("section1:vel_x = %f \n",vel.linear.x);
+            if(vel.linear.x==0.3){
                 check = true;
-            }
-        }
-        if(vel.linear.x > -0.5 && check!=false){
-            vel.linear.x -= 0.1;
-            if(vel.linear.x==-0.5){
-                check = false;
+                printf("check mark true \n");
             }
         }
 
+        if(vel.linear.x > -0.3 && check!=false){
+            vel.linear.x -= 0.1;
+            printf("section2: vel_x = %f \n",vel.linear.x);
+            if(vel.linear.x==-0.3){
+                check = false;
+                printf("check mark false \n");
+            }
+        }
+
+        
         vel_pub_.publish(vel);
 
-        ros::spinOnce();
+        // ros::spinOnce();
 
         loop_rate.sleep();
     }

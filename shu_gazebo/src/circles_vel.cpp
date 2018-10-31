e06ff7b10a4c3ff8173fcec2361e4274adbcb1e9
shulace13 <kozasa0820jps@gmail.com>
2018-10-31 17:25:32 -0400

    Just re-wrote the launch file from diff_drive_gazebo pkg to shu_gazebo pkg. diff_drive_gazebo/launch/main.launch and shu_gazebo/launch/cage_with_robot.launch pretty much does the same exact thing.

--- shu_gazebo/src/circles_vel.cpp
+++ shu_gazebo/src/circles_vel.cpp
@@ -25,34 +25,35 @@ int main(int argc, char **argv){
     geometry_msgs::Twist vel;
     int linear_, angular_;
     double l_scale, a_scale;
-    int check = 0;
+    // int check = 0;
+    bool check = false;
 
     ros::Publisher vel_pub_ = n.advertise<geometry_msgs::Twist>("cmd_vel", 1, true);
-    ros::Rate loop_rate(10);
+    ros::Rate loop_rate(1);
 
     while(ros::ok()){
         linear_ = 1;
-        l_scale = 0.2;
+        l_scale = 0.1;
         angular_ = 1;
-        a_scale = 0.6;
+        a_scale = 0.3;
         
         vel.linear.x = linear_*l_scale;
         vel.angular.z = angular_*a_scale;
-        if(vel.linear.x < 0.5 && check == 0){
+
+        if(vel.linear.x < 0.5 && check!=true){
             vel.linear.x += 0.1;
-            vel_pub_.publish(vel);
             if(vel.linear.x==0.5){
-                check = 1;
+                check = true;
             }
         }
-        if(vel.linear.x > -0.5 && vel.linear.x > 0.5){
+        if(vel.linear.x > -0.5 && check!=false){
             vel.linear.x -= 0.1;
-            vel_pub_.publish(vel);
-            if(vel.linear.x == -0.5){
-                check = 0;
+            if(vel.linear.x==-0.5){
+                check = false;
             }
         }
 
+        vel_pub_.publish(vel);
 
         ros::spinOnce();
 

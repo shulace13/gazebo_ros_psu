c93e71b3494276529c37b9fe03166af1b664ef91
shulace13 <kozasa0820jps@gmail.com>
2018-11-02 17:03:45 -0400

    Discovered that I wasn't able to put part of the odom thing to a new variable. but it does subscribe succesfully. Dull to write a code that goes in squares though. maybe sometime I will

--- shu_gazebo/src/square.cpp
+++ shu_gazebo/src/square.cpp
@@ -26,12 +26,14 @@
 // float VEL_MAX 0.3
 // float VEL_MIN -0.3
 
+/*
 boost::mutex mutex_odom_;
 nav_msgs::Odometry odom_;
 void odom_initCallback(const nav_msgs::Odometry& msg){
     boost::mutex::scoped_lock(mutex_odom_);
     odom_ = msg;
 }
+*/
 
 boost::mutex mutex_odom;
 nav_msgs::Odometry odom;
@@ -47,22 +49,23 @@ void straight(geometry_msgs::Twist &vel){
     vel.angular.z = 0.0;
 }
 
+void turn_left(geometry_msgs::Twist &vel){
+    printf("Turn\n");
+    vel.linear.x = 0.1;
+    vel.angular.z = 0.3;
+}
+
 void stop(geometry_msgs::Twist &vel){
     printf("STOP\n");
     vel.linear.x = 0.0;
     vel.angular.z = 0.0;
 }
 
-void turn_right(geometry_msgs::Twist &vel){
-    printf("Turn\n");
-    vel.linear.x = 0.1;
-    vel.angular.z = 0.3;
-}
-
 int main(int argc, char **argv){
     ros::init(argc, argv, "squares_vel");
     ros::NodeHandle n;
-
+    ros::Rate rate(10);
+    
     geometry_msgs::Twist vel;
     int linear_, angular_;
     double l_scale, a_scale;
@@ -77,6 +80,11 @@ int main(int argc, char **argv){
 
     ros::Publisher vel_pub_ = n.advertise<geometry_msgs::Twist>("cmd_vel", 1, true);
     
+    // Since we assume that the robot starts from the origin of the world
+    // We will take out this section for now (2018/11/02)
+    // It was supposed to remember it's initial pose and then
+    // we can compare the new pose with this init_pose to determine where the robot is.
+    /*
     ros::Subscriber odom_init_sub_ = n.subscribe("/odom",10,odom_initCallback);
     geometry_msgs::Pose init_pose;
     {
@@ -85,41 +93,45 @@ int main(int argc, char **argv){
         init_pose.position.y = odom_.pose.pose.position.y;
         init_pose.position.z = odom_.pose.pose.position.z;
     }
-    
+    */
 
-    ros::Rate loop_rate(3); 
+    ros::Subscriber odom_sub_ = n.subscribe("/odom",10,odomCallback);
 
     while(ros::ok()){
                 
-        printf("Just showing the init pose\nx = %f\ny = %f\nz = %f\n",
-                init_pose.position.x, init_pose.position.y, init_pose.position.z);        
-        ros::Subscriber odom_sub_ = n.subscribe("/odom",10,odomCallback);
-        geometry_msgs::Pose new_pose;
+        // printf("Just showing the init pose\nx = %f\ny = %f\nz = %f\n",
+        //        init_pose.position.x, init_pose.position.y, init_pose.position.z);        
+        // printf("Just showing the init pose\nx = %f\ny = %f\nz = %f\n",
+        //         odom_.pose.pose.position.x, 
+        //         odom_.pose.pose.position.y,
+        //         odom_.pose.pose.position.z); 
+
+        // geometry_msgs::Pose new_pose;
+        nav_msgs::Odometry pose_;
         {
             boost::mutex::scoped_lock(mutex_odom);
-            new_pose.position.x = odom.pose.pose.position.x;
-            new_pose.position.y = odom.pose.pose.position.y;
-            new_pose.position.z = odom.pose.pose.position.z;
+            // new_pose.position.x = odom.pose.pose.position.x;
+            // new_pose.position.y = odom.pose.pose.position.y;
+            // new_pose.position.z = odom.pose.pose.position.z;
+            pose_ = odom;
         }
 
-        printf("Just showing the new pose\nx = %f\ny = %f\nz = %f\n",
-                new_pose.position.x, new_pose.position.y, new_pose.position.z);        
-    
        
-        if(fabs(new_pose.position.x - init_pose.position.x)<0.5){
+        if(pose_.pose.pose.position.x<1.5 && 
+                pose_.pose.pose.position.y<0.1){
             straight(vel);
         }
 
-        if(fabs(new_pose.position.x - init_pose.position.x)>0.5){
-            stop(vel);
-        }        
+        if(pose_.pose.pose.position.x>1.5){
+            turn_left(vel);
+        }
+        
         // printf("Here's linear.x = %f\n", vel.linear.x);
         // printf("Publishing Topics\n");
         vel_pub_.publish(vel);
 
-        // ros::spinOnce();
-
-        loop_rate.sleep();
+        ros::spinOnce();
+        rate.sleep();
     }
 
     return 0;
